4dd15d4759502bb282125773fcca64c32748770b
Dylan Poll <61894413+dylanpoll@users.noreply.github.com>
2022-01-20 11:54:52 -0500

    updated some things
    removed content as I think I over complicated things prior.

--- src/PostgresQuery/QueryForEmailList.ts
+++ src/PostgresQuery/QueryForEmailList.ts
@@ -1,10 +1,9 @@
 import { Client } from "ts-postgres";
 
 export async function QueryForEmailList(columnToSearchFor: string, ID: string) { // https://stackabuse.com/using-postgresql-with-nodejs-and-node-postgres/ and https://www.npmjs.com/package/ts-postgres and  https://node-postgres.com/features/types/
-    var reply: any;
-    var DBPort: any = process.env.POSTGRESQL_PORT;
-    const Port: number = +DBPort;
-    
+    var reply : any;
+    var DBPort : any = process.env.POSTGRESQL_PORT;
+    const Port : number = + DBPort;
     const client = new Client({
         "host": process.env.POSTGRESQL_HOST,
         "port": Port,
@@ -12,18 +11,15 @@ export async function QueryForEmailList(columnToSearchFor: string, ID: string) {
         "user": process.env.POSTGRESQL_USER,
         "password": process.env.POSTGRESQL_PASSWORD
     });
-
     client.connect();
-
-    try { //try is where we form the query and attempt to submit it.
-        const result: any = await client.query("SELECT data -> '" + columnToSearchFor + "' as email FROM useremails WHERE id = " + ID); //made this to funnel any search for the json in the DB
+    try { // try is where we form the query and attempt to submit it.
+        const result : any = await client.query("SELECT data -> '"+columnToSearchFor+"' as email FROM emaillist WHERE id="+ID);//made this to funnel any search for the json in the DB
         await client.end();
-        for await (const row of result) {
-            reply = row.get('email'); //assigns the reply to what we indicated the search is for and attaches it according to the email tag
+        for await (const row of result){
+            reply = row.get("email"); //assigns the reply to what we indicated the search is for and attaches it according to the email tag
         }
         return reply;
     } finally {
         //I honestly don't need this but I am required to have it with this setup, will re work later to get rid of it...
     }
 }
-
--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,22 @@ dotenv.config();
 
 const main = async () => {
     //await newUserQuery();
-    
-    var Difference_in_Days_string = await countDownUtil(); //gets the countdown value
+    let Difference_in_Days_string = await countDownUtil(); //gets the countdown value
     var id = 1;
-
     while(true){
-        let ID : string = ''+id;
-        let RecieverEmail : string | undefined =  await QueryForEmailList( "email", ID); //postgresql query function pulls values from the db dynamically
-        
+        var ID : string = ''+id;
+        var RecieverEmail : string | undefined =  await QueryForEmailList( "email", ID); //postgresql query function pulls values from the db dynamically
         if( RecieverEmail === undefined ){  //this just kills the attempts whenever there is no listed DB value. is set !== can be used to add a new email without sending emails.
             console.log("Email List Returned Undefined when looking for a user at ID: " + id + " .. closing attempt instance as there are no more users to email.");  // just for server logs.
             return false;
         }
-
         else{
             //if(id === 2){  //if statement used to test out particular emails during trouble shooting... don't want to spam everyone on the list.
-            let RecieverClass : string | undefined =  await QueryForEmailList( "class", ID);
-            let RecieverName : string | undefined =  await QueryForEmailList( "name", ID);
-            let subTitleLine : string|undefined = RecieverName + "....";
+            let RecieverClass : string | undefined = await QueryForEmailList( "class", ID);
+            let RecieverName : string | undefined = await QueryForEmailList( "name", ID);
+            let subTitleLine : string | undefined = RecieverName + "....";
             let mailBody : string = htmlEmailBody(Difference_in_Days_string, RecieverName, RecieverClass);  // passes in params to inject into a html template and returns that as a string to be processed by nodeMailer
-            console.log("ID : "+ ID + " | Attempting to send email to : " + RecieverEmail + " | name : " + RecieverName + " | Class : " + RecieverClass);  // just for server logs.
+            console.log("ID : " + ID + " | Attempting to send email to : " + RecieverEmail + " | name : " + RecieverName + " | Class : " + RecieverClass);  // just for server logs.
             sendmail( RecieverEmail , subTitleLine , mailBody );
             //}
         };
